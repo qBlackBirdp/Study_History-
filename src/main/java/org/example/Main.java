2e47bcabc070979c38e0eff058f1b291298edcd0
qBlackbirdp <blackbird9711@gmail.com>
2024-06-04 19:53:38 +0900

    문제 - n부터 m까지의 합을 반환하는 함수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,25 @@
 package org.example;
 // 문제 : 아래와 같이 출력 되도록 해주세요.
 
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
-        // 예시 시작
-//        int c = 20 + Math.더하기(10, 20);
-//        System.out.println(c);
-        // 예시 끝
-        Scanner sc = new Scanner(System.in);
-        System.out.println("1부터 n까지의 합,,, n 값을 입력하면 구해줌.");
-
-        int i = sc.nextInt();
-
-        System.out.printf("1부터 %d까지의 합\n", i);
-        int 결과1 = Math.oneToSum(i);
+        int 결과1 = Math.nToMSum(2, 3);
         System.out.println("결과1 : " + 결과1);
-        // 출력 : 결과1 : 6
+        // 출력 : 결과1 : 5
 
-        System.out.println("============================================");
-        System.out.println("1부터 n까지의 합,,, n 값을 입력하면 구해줌.");
-
-        i = sc.nextInt();
-        System.out.printf("1부터 %d까지의 합\n", i);
-        int 결과2 = Math.oneToSum(i);
+        int 결과2 = Math.nToMSum(5, 10);
         System.out.println("결과2 : " + 결과2);
-        // 출력 : 결과2 : 55
+        // 출력 : 결과2 : 45
     }
 }
 
 class Math {
-//    static int 더하기(int a, int b) {
-//        int c = a + b;
-//
-//        return c;
-//    }
-
-    public static int oneToSum(int i) {
-        int sum = 0;
-        while (0 <= i) {
-            sum += i;
-            i--;
+    static int nToMSum (int x, int y){
+        int sum = x;
+        x = x + 1;
+        while (x <= y){
+            sum = sum + x;
+            x++;
         }
         return sum;
     }
