7ea6dee35231b21b9df047950a0f14c5cfb7aefb
qBlackbirdp <blackbird9711@gmail.com>
2024-06-04 19:58:11 +0900

    문제 - n부터 m까지의 합을 반환하는 함수, 입력까지 받기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,27 @@
 package org.example;
 // 문제 : 아래와 같이 출력 되도록 해주세요.
 
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        int 결과1 = Math.nToMSum(2, 3);
+        Scanner sc = new Scanner(System.in);
+        int x, y;
+        System.out.println("x부터 y의 합을 알려주겠다.");
+        System.out.println("x입력");
+        x = sc.nextInt();
+        System.out.println("y입력");
+        y = sc.nextInt();;
+        int 결과1 = Math.nToMSum(x, y);
         System.out.println("결과1 : " + 결과1);
         // 출력 : 결과1 : 5
-
-        int 결과2 = Math.nToMSum(5, 10);
+        System.out.println("==========================");
+        System.out.println("x부터 y의 합을 알려주겠다.");
+        System.out.println("x입력");
+        x = sc.nextInt();
+        System.out.println("y입력");
+        y = sc.nextInt();
+        int 결과2 = Math.nToMSum(x, y);
         System.out.println("결과2 : " + 결과2);
         // 출력 : 결과2 : 45
     }
