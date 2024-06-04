e4438b8c350a1c9b39b7ca38a8dfd9f4ae12028f
qBlackbirdp <blackbird9711@gmail.com>
2024-06-04 16:38:06 +0900

    입력까지 받아보기, return개념 이해

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,21 +5,30 @@ import java.util.Scanner;
 class Main {
 
     public static void main(String[] args) {
-        boolean rs = 계산기.is_a_bigger_than_b(10,20);
-        System.out.println(rs);
-
-        rs = 계산기.is_a_bigger_than_b(240,20);
-        System.out.println(rs);
-
-        rs = 계산기.is_a_bigger_than_b(20,20);
+//        boolean rs = 계산기.is_a_bigger_than_b(10,20);
+//        System.out.println(rs);
+//
+//        rs = 계산기.is_a_bigger_than_b(240,20);
+//        System.out.println(rs);
+//
+//        rs = 계산기.is_a_bigger_than_b(20,20);
+//        System.out.println(rs);
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("a는 b보다 큰가?");
+        System.out.println("a 값 입력");
+        a = sc.nextInt();
+        System.out.println("b 값 입력");
+        b = sc.nextInt();
+        boolean rs = 계산기.is_a_bigger_than_b(a, b);
         System.out.println(rs);
     }
 }
 
 class 계산기 {
 
-    static boolean is_a_bigger_than_b (int a, int b) {
-
+    static boolean is_a_bigger_than_b(int a, int b) {
+        System.out.printf("%d > %d : ", a , b);
         return a > b;
     }
 }
