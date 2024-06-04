cc533a704cfc362e8687f8d52b45f82e5d70ff76
qBlackbirdp <blackbird9711@gmail.com>
2024-06-04 18:32:07 +0900

    함수문제 void 없이.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,34 +4,39 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        계산기.더한결과를_화면에_출력(10, 20);
+        int a = 계산기.더한결과를_화면에_출력(10, 20);
+        System.out.println(a);
         // 출력 => 결과 : 30
 
-        계산기.더한결과를_화면에_출력(50, 20);
+        int b = 계산기.더한결과를_화면에_출력(50, 20);
+        System.out.println(b);
         // 출력 => 결과 : 70
 
-        계산기.뺀결과를_화면에_출력(50, 20);
+        int c = 계산기.뺀결과를_화면에_출력(50, 20);
+        System.out.println(c);
         // 출력 => 결과 : 30
 
-        계산기.뺀결과를_화면에_출력(5, 2);
+        int d = 계산기.뺀결과를_화면에_출력(5, 2);
+        System.out.println(d);
         // 출력 => 결과 : 3
 
-        계산기.곱한결과를_화면에_출력(5, 2);
+        int e = 계산기.곱한결과를_화면에_출력(5, 2);
+        System.out.println(e);
         // 출력 => 결과 : 10
     }
 }
 
 class 계산기 {
 
-    public static void 더한결과를_화면에_출력(int a, int b) {
-        System.out.println(a + b);
+    public static int 더한결과를_화면에_출력(int a, int b) {
+        return a + b;
     }
 
-    public static void 뺀결과를_화면에_출력(int a, int b) {
-        System.out.println(a - b);
+    public static int 뺀결과를_화면에_출력(int a, int b) {
+        return a - b;
     }
 
-    public static void 곱한결과를_화면에_출력(int a, int b) {
-        System.out.println(a * b);
+    public static int 곱한결과를_화면에_출력(int a, int b) {
+        return a * b;
     }
 }
