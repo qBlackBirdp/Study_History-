b7e3c17bfed8fd64df2225892f183f75abb75638
qBlackbirdp <blackbird9711@gmail.com>
2024-06-04 11:25:56 +0900

    차 이름과 속력 입력후 최고속력 출력하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,63 +4,43 @@ package org.example;
 
 import java.util.Scanner;
 
-class Main {
+
+// 문제 : 번호가 다른 각각의 자동차가 서로 다른 최고속력으로 달리게 해주세요.
+class Main { // main 클래스
     public static void main(String[] args) {
-        bandMember[] bm = new bandMember[5]; //=> 반복문 활용을 위한 변수 선언 (5명이라 5개 만듦)
         Scanner sc = new Scanner(System.in);
+        // 구현시작
+        car aCar = new car(); // -> aCar 선언
+        car bCar = new car(); // -> bCar 선언
+        System.out.println("두대의 차의 최고 속력을 출력해줍니다.");
 
-        bandMember v = new bandMember();
-        bm[0] = v;
-        v.name = sc.next(); // -> 인스턴스가 존재하지 않으면 오류, 클래스 안에 객체를 생성해야함.
-        v.position = sc.next();
-        v.age = sc.nextInt();
-        v.isMarred = sc.nextBoolean();
+        System.out.println("첫번째 차의 이름을 입력해주세요.");
+        aCar.name = sc.next();
 
-        bandMember p = new bandMember();
-        bm[1] = p;
-        p.name = "김모씨";
-        p.position = "Piano";
-        p.age = 25;
-        p.isMarred = false;
+        System.out.println("두번째 차의 이름을 입력해주세요.");
+        bCar.name = sc.next();
 
-        bandMember g = new bandMember();
-        bm[2] = g;
-        g.name = "유모씨";
-        g.position = "Guitar";
-        g.age = 27;
-        g.isMarred = false;
 
-        bandMember b = new bandMember();
-        bm[3] = b;
-        b.name = "한모씨";
-        b.position = "Bass";
-        b.age = 28;
-        b.isMarred = false;
+        System.out.println("a Car 의 최고 속력을 입력해주세요.");
+        aCar.maxKm = sc.nextInt();
+        System.out.println("b Car 의 최고 속력을 입력해주세요.");
 
-        bandMember d = new bandMember();
-        bm[4] = d;
-        d.name = "오모씨";
-        d.position = "Drum";
-        d.age = 29;
-        d.isMarred = false;
+        bCar.maxKm = sc.nextInt();
 
-        for (int i = 0; i <= 4; i++) {
-            bandMember a = bm[i]; // -> 반복할 때 마다 a 값 0~4까지.
-            bm[i] = a;
-            a.introduce();
-        }
+//        aCar.maxKm = 200; // -> aCar 최고 속력 데이터 저장
+//        bCar.maxKm = 240; // -> bCar 최고 속력 데이터 저장
 
+        aCar.drive(); // -> 메서드 호출 (함수 실행) -> 구성요소중 기능을 담당
+
+        bCar.drive(); // -> 메서드 호출 (함수 실행)
     }
 }
 
-class bandMember {
+class car {
+    int maxKm;
     String name;
-    String position;
-    int age;
-    boolean isMarred;
 
-    void introduce() {
-        System.out.printf("이름: %s\n파트: %s\n나이: %d\n결혼여부: %b\n\n", this.name, this.position, this.age, this.isMarred);//-> a변수를 출력문에 대입.
-
+    void drive() { // -> 실행버튼(기능) -> 메서드 -> 기능을 담당
+        System.out.printf("%s가(이) 최고속력 %dkm/h로 달립니다.\n", this.name, this.maxKm);
     }
 }
