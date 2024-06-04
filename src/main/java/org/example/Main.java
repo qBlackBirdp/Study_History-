13cfe6a7f92dd95e4673b9240fba0517cce2e088
qBlackbirdp <blackbird9711@gmail.com>
2024-06-04 18:51:16 +0900

    함수문제 입력까지 받아보기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,24 +4,57 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        int a = 계산기.더한결과를_화면에_출력(10, 20);
-        System.out.println(a);
-        // 출력 => 결과 : 30
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        System.out.println("=============");
+        System.out.println("더하기입니다.");
+        System.out.println("a 값입력");
+        a = sc.nextInt();
+        System.out.println("b 값입력");
+        b = sc.nextInt();
 
-        int b = 계산기.더한결과를_화면에_출력(50, 20);
-        System.out.println(b);
+        int x = 계산기.더한결과를_화면에_출력(a, b);
+        System.out.printf("%d + %d = %d \n", a, b, x);
+        // 출력 => 결과 : 30
+        System.out.println("=============");
+        System.out.println("더하기입니다.");
+        System.out.println("a 값입력");
+        a = sc.nextInt();
+        System.out.println("b 값입력");
+        b = sc.nextInt();
+
+        int y = 계산기.더한결과를_화면에_출력(a, b);
+        System.out.printf("%d + %d = %d \n", a, b, y);
         // 출력 => 결과 : 70
+        System.out.println("=============");
+        System.out.println("빼기입니다.");
+        System.out.println("a 값입력");
+        a = sc.nextInt();
+        System.out.println("b 값입력");
+        b = sc.nextInt();
 
-        int c = 계산기.뺀결과를_화면에_출력(50, 20);
-        System.out.println(c);
+        int c = 계산기.뺀결과를_화면에_출력(a, b);
+        System.out.printf("%d - %d = %d \n", a, b, c);
         // 출력 => 결과 : 30
+        System.out.println("=============");
+        System.out.println("빼기입니다.");
+        System.out.println("a 값입력");
+        a = sc.nextInt();
+        System.out.println("b 값입력");
+        b = sc.nextInt();
 
-        int d = 계산기.뺀결과를_화면에_출력(5, 2);
-        System.out.println(d);
+        int d = 계산기.뺀결과를_화면에_출력(a, b);
+        System.out.printf("%d - %d = %d \n", a, b, d);
         // 출력 => 결과 : 3
+        System.out.println("=============");
+        System.out.println("곱하기입니다.");
+        System.out.println("a 값입력");
+        a = sc.nextInt();
+        System.out.println("b 값입력");
+        b = sc.nextInt();
 
-        int e = 계산기.곱한결과를_화면에_출력(5, 2);
-        System.out.println(e);
+        int e = 계산기.곱한결과를_화면에_출력(a, b);
+        System.out.printf("%d * %d = %d \n", a, b, e);
         // 출력 => 결과 : 10
     }
 }
