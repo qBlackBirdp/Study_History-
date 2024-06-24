987bdfb438972af0e3262a67cc92f9a1fb7821e4
qBlackbirdp <blackbird9711@gmail.com>
2024-06-24 09:37:37 +0900

    꼬마 정민 문제풀이

--- src/main/java/org/example2/Main2.java
+++ src/main/java/org/example2/Main2.java
@@ -1,28 +1,21 @@
 package org.example2;
 
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class Main2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = 0;
+        String[] inputs = sc.nextLine().split(" ");
 
-        while (true) {
-            System.out.print("숫자: ");
-            try {
-                n = sc.nextInt();  // 대기, 숫자를 똑바로 입력할 때 까지
-                sc.nextLine(); //버퍼를 지운다.
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("숫자를 똑바로 입력해.");
-                sc.nextLine(); //버퍼를 지운다.
-            }
+        long sum = 0;
+
+        for (String inputData : inputs) {
+            sum += Long.parseLong(inputData);
         }
 
-        System.out.printf("입력된 숫자 : %d", n);
+        System.out.println(sum);
 
         sc.close();
     }
